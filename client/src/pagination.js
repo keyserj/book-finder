cda666065933db5e7790c8ab764878519dc0f53b
Joel Keyser <keyser.joel@gmail.com>
2019-02-05 19:08:46 -0600

    Add logic for pagination without tying to UI

---
+++ client/src/pagination.js
@@ -0,0 +1,146 @@
+const PREVIOUS = -2;
+const NEXT = -1;
+const RESULTS_PER_PAGE = 10;
+const MAX_PAGE_DISTANCE_FROM_SELECTED = 2;
+
+/**
+ * @param {JQuery<HTMLElement>} pageItemTemplate 
+ * @param {number} firstResultNumber 
+ * @param {number} lastResultNumber 
+ * @param {number} totalResults 
+ */
+export function insertPageItems(
+  pageItemTemplate,
+  firstResultNumber,
+  lastResultNumber,
+  totalResults) {
+
+  validate(pageItemTemplate, firstResultNumber, lastResultNumber, totalResults);
+
+  const selectedPageNumber =
+    Math.floor((lastResultNumber - 1) / RESULTS_PER_PAGE) + 1;
+
+  const pageNumbers =
+    getPageNumbers(
+      selectedPageNumber,
+      firstResultNumber,
+      lastResultNumber,
+      totalResults);
+
+  if (pageNumbers.length > 1) {
+    let elementToInsertAfter = pageItemTemplate;
+
+    for (let i = 0; i < pageNumbers.length; i++) {
+      const isSelectedPage = pageNumbers[i] === selectedPageNumber;
+      const pageItem = createPageItem(pageItemTemplate, pageNumbers[i], isSelectedPage);
+      pageItem.insertAfter(elementToInsertAfter);
+      elementToInsertAfter = pageItem;
+    }
+  }
+}
+
+/**
+ * @param {JQuery<HTMLElement>} pageItemTemplate 
+ * @param {number} firstResultNumber 
+ * @param {number} lastResultNumber 
+ * @param {number} totalResults 
+ */
+function validate(pageItemTemplate, firstResultNumber, lastResultNumber, totalResults) {
+  if (!pageItemTemplate || !firstResultNumber || !lastResultNumber || !totalResults) {
+    throw new Error('No argument can be null');
+  }
+  else if (!pageItemTemplate.length) {
+    throw new Error('Empty pageItemTemplate cannot be used to create page items');
+  }
+  else if (!isOnesDigitAOne(firstResultNumber)) {
+    throw new Error('The ones digit of the first page result should be a one');
+  }
+  else if (lastResultNumber >= (firstResultNumber + RESULTS_PER_PAGE)) {
+    throw new Error(`Last page result should not be ${RESULTS_PER_PAGE}+` +
+      ' pages higher than first page result');
+  } else if (totalResults < (lastResultNumber - firstResultNumber + 1)) {
+    throw new Error('Total results cannot be smaller than last result - first result + 1');
+  }
+}
+
+/**
+ * @param {number} firstResultNumber 
+ */
+function isOnesDigitAOne(firstResultNumber) {
+  return firstResultNumber.toString().slice(-1) === '1';
+}
+
+/**
+ * @param {JQuery<HTMLElement>} pageItemTemplate 
+ * @param {number} pageNumber 
+ * @param {Boolean} isSelectedPage 
+ * @returns {JQuery<HTMLElement>}
+ */
+function createPageItem(pageItemTemplate, pageNumber, isSelectedPage) {
+  const pageItemClone = pageItemTemplate.clone();
+  pageItemClone.removeClass('d-none');
+
+  if (isSelectedPage) {
+    pageItemClone.addClass('selected');
+  }
+
+  const aTagOfPageItem = pageItemClone.find('a');
+  aTagOfPageItem.text(getPageNumberAsString(pageNumber));
+
+  return pageItemClone;
+}
+
+/**
+ * @param {number} pageNumber 
+ * @return {string}
+ */
+function getPageNumberAsString(pageNumber) {
+  let pageNumberAsString;
+
+  if (pageNumber === PREVIOUS) {
+    pageNumberAsString = 'Previous';
+  } else if (pageNumber === NEXT) {
+    pageNumberAsString = 'Next';
+  } else {
+    pageNumberAsString = pageNumber.toString();
+  }
+
+  return pageNumberAsString;
+}
+
+/**
+ * @param {number} selectedPageNumber 
+ * @param {number} firstResultNumber 
+ * @param {number} lastResultNumber 
+ * @param {number} totalResults 
+ * @returns {number[]}
+ */
+function getPageNumbers(
+  selectedPageNumber,
+  firstResultNumber,
+  lastResultNumber,
+  totalResults) {
+
+  const pageNumbers = [selectedPageNumber];
+
+  const numberOfPagesBeforeSelected =
+    Math.floor((firstResultNumber - 1)) / RESULTS_PER_PAGE;
+  const numberOfPagesAfterSelected =
+    Math.ceil((totalResults - lastResultNumber) / RESULTS_PER_PAGE);
+
+  for (let i = 1; i <= numberOfPagesBeforeSelected && i <= MAX_PAGE_DISTANCE_FROM_SELECTED; i++) {
+    pageNumbers.unshift(selectedPageNumber - i);
+  }
+  if (numberOfPagesBeforeSelected > 0) {
+    pageNumbers.unshift(PREVIOUS);
+  }
+
+  for (let i = 1; i <= numberOfPagesAfterSelected && i <= MAX_PAGE_DISTANCE_FROM_SELECTED; i++) {
+    pageNumbers.push(selectedPageNumber + i);
+  }
+  if (numberOfPagesAfterSelected > 0) {
+    pageNumbers.push(NEXT);
+  }
+
+  return pageNumbers;
+}
---
+++ client/tests/pagination.test.js
@@ -0,0 +1,203 @@
+/**
+ * Notes:
+ * When referring to pages, an asterisk (*) means that page is selected
+ * e.g. 'page 1*' refers to the first page, which is selected.
+ * 
+ * When referring to pages, an 'n' refers to the 'next' page item and a 'p'
+ * refers to the 'previous' page item.
+ */
+import $ from 'jquery';
+import { insertPageItems } from '../src/pagination';
+
+const templateHTML =
+  '<li class="page-item d-none"><a class="page-link" href="#"></a></li>';
+const jqueryTemplateHTML = $(templateHTML);
+
+test('inserts no pageItems if 10 or fewer results', () => {
+  const expectedNextElement = $(jqueryTemplateHTML).next();
+
+  insertPageItems(jqueryTemplateHTML, 1, 10, 10);
+
+  const actualNextPage = jqueryTemplateHTML.next();
+  expect(actualNextPage).toEqual(expectedNextElement);
+})
+
+test('inserts pages 1*,2,n if displaying 1-10 of 11 total results', () => {
+  const jqueryExpectedPage1 =
+    $('<li class="page-item selected"><a class="page-link" href="#">1</a></li>');
+  const jqueryExpectedPage2 =
+    $('<li class="page-item"><a class="page-link" href="#">2</a></li>');
+  const jqueryExpectedPagen =
+    $('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
+
+  insertPageItems(jqueryTemplateHTML, 1, 10, 11);
+
+  const actualNextPage1 = jqueryTemplateHTML.next();
+  const actualNextPage2 = actualNextPage1.next();
+  const actualNextPagen = actualNextPage2.next();
+  expect(actualNextPage1).toEqual(jqueryExpectedPage1);
+  expect(actualNextPage2).toEqual(jqueryExpectedPage2);
+  expect(actualNextPagen).toEqual(jqueryExpectedPagen);
+})
+
+test('inserts pages p,1,2* if displaying 11-11 of 11 total results', () => {
+  const jqueryExpectedPagep =
+    $('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+  const jqueryExpectedPage1 =
+    $('<li class="page-item"><a class="page-link" href="#">1</a></li>');
+  const jqueryExpectedPage2 =
+    $('<li class="page-item selected"><a class="page-link" href="#">2</a></li>');
+
+  insertPageItems(jqueryTemplateHTML, 11, 11, 11);
+
+  const actualNextPagep = jqueryTemplateHTML.next();
+  const actualNextPage1 = actualNextPagep.next();
+  const actualNextPage2 = actualNextPage1.next();
+  expect(actualNextPagep).toEqual(jqueryExpectedPagep);
+  expect(actualNextPage1).toEqual(jqueryExpectedPage1);
+  expect(actualNextPage2).toEqual(jqueryExpectedPage2);
+})
+
+test('inserts pages p,1,2,3*,4,5,n if displaying 21-30 of 50 total results', () => {
+  const jqueryExpectedPagep =
+    $('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+  const jqueryExpectedPage1 =
+    $('<li class="page-item"><a class="page-link" href="#">1</a></li>');
+  const jqueryExpectedPage2 =
+    $('<li class="page-item"><a class="page-link" href="#">2</a></li>');
+  const jqueryExpectedPage3 =
+    $('<li class="page-item selected"><a class="page-link" href="#">3</a></li>');
+  const jqueryExpectedPage4 =
+    $('<li class="page-item"><a class="page-link" href="#">4</a></li>');
+  const jqueryExpectedPage5 =
+    $('<li class="page-item"><a class="page-link" href="#">5</a></li>');
+  const jqueryExpectedPagen =
+    $('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
+
+  insertPageItems(jqueryTemplateHTML, 21, 30, 50);
+
+  const actualNextPagep = jqueryTemplateHTML.next();
+  const actualNextPage1 = actualNextPagep.next();
+  const actualNextPage2 = actualNextPage1.next();
+  const actualNextPage3 = actualNextPage2.next();
+  const actualNextPage4 = actualNextPage3.next();
+  const actualNextPage5 = actualNextPage4.next();
+  const actualNextPagen = actualNextPage5.next();
+  expect(actualNextPagep).toEqual(jqueryExpectedPagep);
+  expect(actualNextPage1).toEqual(jqueryExpectedPage1);
+  expect(actualNextPage2).toEqual(jqueryExpectedPage2);
+  expect(actualNextPage3).toEqual(jqueryExpectedPage3);
+  expect(actualNextPage4).toEqual(jqueryExpectedPage4);
+  expect(actualNextPage5).toEqual(jqueryExpectedPage5);
+  expect(actualNextPagen).toEqual(jqueryExpectedPagen);
+})
+
+// inserts pages p,1,2,3*,4,5,n if displaying 21-30 of 100 total results
+test('inserts at most two pages above the selected page', () => {
+  const jqueryExpectedPagep =
+    $('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+  const jqueryExpectedPage1 =
+    $('<li class="page-item"><a class="page-link" href="#">1</a></li>');
+  const jqueryExpectedPage2 =
+    $('<li class="page-item"><a class="page-link" href="#">2</a></li>');
+  const jqueryExpectedPage3 =
+    $('<li class="page-item selected"><a class="page-link" href="#">3</a></li>');
+  const jqueryExpectedPage4 =
+    $('<li class="page-item"><a class="page-link" href="#">4</a></li>');
+  const jqueryExpectedPage5 =
+    $('<li class="page-item"><a class="page-link" href="#">5</a></li>');
+  const jqueryExpectedPagen =
+    $('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
+
+  insertPageItems(jqueryTemplateHTML, 21, 30, 100);
+
+  const actualNextPagep = jqueryTemplateHTML.next();
+  const actualNextPage1 = actualNextPagep.next();
+  const actualNextPage2 = actualNextPage1.next();
+  const actualNextPage3 = actualNextPage2.next();
+  const actualNextPage4 = actualNextPage3.next();
+  const actualNextPage5 = actualNextPage4.next();
+  const actualNextPagen = actualNextPage5.next();
+  expect(actualNextPagep).toEqual(jqueryExpectedPagep);
+  expect(actualNextPage1).toEqual(jqueryExpectedPage1);
+  expect(actualNextPage2).toEqual(jqueryExpectedPage2);
+  expect(actualNextPage3).toEqual(jqueryExpectedPage3);
+  expect(actualNextPage4).toEqual(jqueryExpectedPage4);
+  expect(actualNextPage5).toEqual(jqueryExpectedPage5);
+  expect(actualNextPagen).toEqual(jqueryExpectedPagen);
+})
+
+// inserts pages p,2,3,4*,5,n if displaying 31-40 of 50 total results
+test('inserts at most two pages below the selected page', () => {
+  const jqueryExpectedPagep =
+    $('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+  const jqueryExpectedPage2 =
+    $('<li class="page-item"><a class="page-link" href="#">2</a></li>');
+  const jqueryExpectedPage3 =
+    $('<li class="page-item"><a class="page-link" href="#">3</a></li>');
+  const jqueryExpectedPage4 =
+    $('<li class="page-item selected"><a class="page-link" href="#">4</a></li>');
+  const jqueryExpectedPage5 =
+    $('<li class="page-item"><a class="page-link" href="#">5</a></li>');
+  const jqueryExpectedPagen =
+    $('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
+
+  insertPageItems(jqueryTemplateHTML, 31, 40, 50);
+
+  const actualNextPagep = jqueryTemplateHTML.next();
+  const actualNextPage2 = actualNextPagep.next();
+  const actualNextPage3 = actualNextPage2.next();
+  const actualNextPage4 = actualNextPage3.next();
+  const actualNextPage5 = actualNextPage4.next();
+  const actualNextPagen = actualNextPage5.next();
+  expect(actualNextPagep).toEqual(jqueryExpectedPagep);
+  expect(actualNextPage2).toEqual(jqueryExpectedPage2);
+  expect(actualNextPage3).toEqual(jqueryExpectedPage3);
+  expect(actualNextPage4).toEqual(jqueryExpectedPage4);
+  expect(actualNextPage5).toEqual(jqueryExpectedPage5);
+  expect(actualNextPagen).toEqual(jqueryExpectedPagen);
+})
+
+test('throws exception if the ones digit of firstPageNumber is not 1', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 0, 10, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 2, 10, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 9, 10, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 10, 10, 10)).toThrow();
+
+  expect(() => insertPageItems(jqueryTemplateHTML, 30, 40, 40)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 32, 40, 40)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 39, 40, 40)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 40, 40, 40)).toThrow();
+})
+
+test('throws exception if the lastPageNumber is 10+ bigger than first', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 1, 11, 11)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 11, 30, 30)).toThrow();
+})
+
+test('throws exception if firstResultNumber is greater than lastResultNumber', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 2, 1, 11)).toThrow();
+})
+
+test('throws exception if firstResultNumber is negative', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 2, 1, 11)).toThrow();
+})
+
+test('throws exception if any parameter is null', () => {
+  expect(() => insertPageItems(null, 1, 10, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, null, 10, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 1, null, 10)).toThrow();
+  expect(() => insertPageItems(jqueryTemplateHTML, 1, 10, null)).toThrow();
+})
+
+test('throws exception if template is empty', () => {
+  expect(() => insertPageItems($('blah'), 1, 10, 10)).toThrow();
+})
+
+test('throws exception if totalResults is negative', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 1, 10, -1)).toThrow();
+})
+
+test('throws exception if totalResults is smaller than lastResult - firstResult + 1', () => {
+  expect(() => insertPageItems(jqueryTemplateHTML, 1, 10, 9)).toThrow();
+})
